2652c89c218d19ece4795413ac40976089a8f538
Ammar <ammarhussain0315@gmail.com>
2023-12-21 23:25:31 +0500

    lll

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d743ee836b3a9171"; }
+/******/ 		__webpack_require__.h = function() { return "5e6067b6b8c69e88"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/companydetails/page.jsx
+++ src/app/companydetails/page.jsx
@@ -16,6 +16,9 @@ import React, { useState, useEffect } from 'react';
 import CustomModal from '@/components/CustomModal';
 import SvgIcons from '@/assets/SvgIcons';
 import axios from 'axios';
+import Flag from 'react-flags-select';
+// import 'react-flags-select/css/react-flags-select.css';
+import { US } from 'country-flag-icons/react/3x2'
 
 const Page = () => {
   const [activeTab, setActiveTab] = useState('timeline');
@@ -23,6 +26,29 @@ const Page = () => {
   const [show, setShow] = useState(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const monthsOfYear = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
   const handleTabClick = (tab) => {
     setActiveTab(tab);
     // Add any additional functionality you want to execute when a tab is clicked
@@ -33,8 +59,20 @@ const Page = () => {
     axios
       .get(apiUrl)
       .then((response) => {
-        setData(response.data.data);
-        console.log('response.data', response.data.data);
+        const codeMaxTimeMap = new Map();
+        response.data.data.forEach((data) => {
+          const { sepratecode, timeSpent } = data;
+          if (
+            !codeMaxTimeMap.has(sepratecode) ||
+            timeSpent > codeMaxTimeMap.get(sepratecode).timeSpent
+          ) {
+            codeMaxTimeMap.set(sepratecode, { ...data });
+          }
+        });
+
+        const result = Array.from(codeMaxTimeMap.values());
+        setData(result);
+        console.log('result', result);
       })
       .catch((error) => {
         setError(error.message);
@@ -155,67 +193,86 @@ const Page = () => {
               <div>
                 {data &&
                   data.length > 0 &&
-                  data.map((ele,idx) => {
-                   return  <div className=' max-w-[600px] w-full mx-3 my-4 bg-white border-2  border-solid  p-5 px-3'>
-                      <div className='flex gap-2 max-w-[570px] justify-between items-center px-2 py-2 bg-white w-full '>
-                        <div className='flex gap-2 items-center'>
-                          <p className='text-4xl'>18</p>
+                  data.map((ele, idx) => {
+                    return (
+                      <div className=' max-w-[600px] w-full mx-3 my-4 bg-white border-2  border-solid  p-5 px-3'>
+                        <div className='flex gap-2 max-w-[570px] justify-between items-center px-2 py-2 bg-white w-full '>
+                          <div className='flex gap-2 items-center'>
+                            <p className='text-4xl'>
+                              {new Date(ele.date).getDate()}
+                            </p>
+                            <div>
+                              <p>{daysOfWeek[new Date(ele.date).getDay()]}</p>
+                              <p>
+                                {monthsOfYear[new Date(ele.date).getMonth()]},{' '}
+                                {new Date(ele.date).getFullYear()}
+                              </p>
+                            </div>
+                          </div>
                           <div>
-                            <p>Thursday</p>
-                            <p>September, 2023</p>
+                            {/* <p>7:24 PM</p> */}
+                            <p>
+                              {new Intl.DateTimeFormat('en-US', {
+                                hour: 'numeric',
+                                minute: 'numeric',
+                                hour12: true,
+                              }).format(new Date(ele.date))}
+                            </p>
+                          </div>
+
+                          <div className='flex gap-4'>
+                            <Image
+                              src={uslogoicon}
+                              alt='uslogo'
+                              className='w-[24px] h-[20px]'
+                            />
+                           {/* <US title="United States" className="..."/> */}
+                            {/* <p>{ele.ip.country}</p> */}
+                          </div>
+
+                          <div className=' px-2 py-1 border border-solid'>
+                            Jack Caspino
                           </div>
                         </div>
-                        <div>
-                          <p>7:24 PM</p>
-                        </div>
 
-                        <div className='flex gap-4'>
-                          <Image
-                            src={uslogoicon}
-                            alt='uslogo'
-                            className='w-[24px] h-[20px]'
-                          />
-                          <p>{ele.ip.country}</p>
+                        <div className='max-w-[500px] bg-white mx-auto w-full grid grid-cols-4 text-[#979797] place-content-between place-items-center gap-3'>
+                          <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
+                            <p className='text-[10px] text-center'>Client</p>
+                            <p className='text-[10px] pt-10 text-center'>
+                              Chrome, Desktop
+                            </p>
+                          </div>
+                          <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
+                            <p className='text-[10px] text-center'>Referrer</p>
+                            <p className='text-[10px] pt-10 text-center'>
+                              {ele?.referrer ? 'reffered' : 'direct'}
+                            </p>
+                          </div>
+                          <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
+                            <p className='text-[10px] text-center'>
+                              Pages Viewed
+                            </p>
+                            <p className='text-[10px] pt-10 text-center'>2</p>
+                          </div>
+                          <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
+                            <p className='text-[10px] text-center'>
+                              Time Spent
+                            </p>
+                            <p className='text-[10px] pt-10 text-center'>
+                              {parseInt(ele.timeSpent / 60000)} mins
+                            </p>
+                          </div>
                         </div>
-
-                        <div className=' px-2 py-1 border border-solid'>
-                          Jack Caspino
-                        </div>
-                      </div>
-
-                      <div className='max-w-[500px] bg-white mx-auto w-full grid grid-cols-4 text-[#979797] place-content-between place-items-center gap-3'>
-                        <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
-                          <p className='text-[10px] text-center'>Client</p>
-                          <p className='text-[10px] pt-10 text-center'>
-                            Chrome, Desktop
+                        <div className='max-w-[500px] bg-white border border-[#979797] my-2 mx-auto w-full  text-black  '>
+                          <p className=' px-3 py-2 text-xl'>
+                            {ele.pageName.replace('/', '')}
                           </p>
-                        </div>
-                        <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
-                          <p className='text-[10px] text-center'>Referrer</p>
-                          <p className='text-[10px] pt-10 text-center'>
-                            {ele?.referrer || ''}
-                          </p>
-                        </div>
-                        <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
-                          <p className='text-[10px] text-center'>
-                            Pages Viewed
-                          </p>
-                          <p className='text-[10px] pt-10 text-center'>2</p>
-                        </div>
-                        <div className='bg-[#f7f7f7] text-[#979797]  w-[120px] p-3'>
-                          <p className='text-[10px] text-center'>Time Spent</p>
-                          <p className='text-[10px] pt-10 text-center'>
-                            6 mins
+                          <p className='text-[#979797] px-3'>
+                            For {ele.timeSpent / 1000} seconds
                           </p>
                         </div>
                       </div>
-                      <div className='max-w-[500px] bg-white border border-[#979797] my-2 mx-auto w-full  text-black  '>
-                        <p className=' px-3 py-2 text-xl'>
-                          Visited /marketing/
-                        </p>
-                        <p className='text-[#979797] px-3'>For 1200 seconds</p>
-                      </div>
-                    </div>;
+                    );
                   })}
               </div>
             </div>
