a5d587c74520dfa24a9f30867977bd5c6c523588
Ammar <ammarhussain0315@gmail.com>
2023-12-29 00:32:05 +0500

    delete old

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Zb1I3DfbEYhtKV4tLMzBt8xnfye/bh+PUbeJtmauipA=\"}"
--- build/server/server-reference-manifest.js
+++ build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"OQeqvu37TPiz4q5PEgsjelQ9mOoT1luVo7DISA+EIj4=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ozb/9lJoulbRZES2z0WmJHpiF9pEDbZCXTz0rhRrx6Y=\"\n}"
